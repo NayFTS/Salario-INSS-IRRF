f27d2acdb6ce7dddc0e430156b47593a08ab0ab1
Nathália Freitas Menezes <nathaliafmenezes.ti@gmail.com>
2019-03-14 14:39:36 -0300

    Update TESTE SALARIO.py

--- TESTE SALARIO.py
+++ TESTE SALARIO.py
@@ -10,16 +10,18 @@ class SalarioTest(unittest.TestCase):
         self.assertEqual(4620, salario.salario_bruto)
 
 
-    #def test_inss(self):
-     #   salario = Salario_1(4620)
-      #  self.assertAlmostEqual(508.205, salario.inss1)
+    def test_inss(self):
+        salario = Salario_1(4620)
+        self.assertEqual(508.205, salario.inss1())
 
     def test_salario_com_contribuicao(self):
         salario = Salario_1(4620)
-        self.assertAlmostEqual(4111.795, salario.salario_com_contribuicao)
+        self.assertEqual(4111.795, salario.salario_com_contribuicao())
+
     def test_irrf1(self):
         salario = Salario_1(4620)
-        self.assertAlmostEqual(289.02875, salario.irrf1)
+        self.assertEqual(289.0288750000001, salario.irrf1())
+
     def test_salario_liquido(self):
         salario = Salario_1(4620)
-        self.assertAlmostEqual(3822.771, salario.salarioliquido)
+        self.assertEqual(3822.766125, salario.salarioliquido())
